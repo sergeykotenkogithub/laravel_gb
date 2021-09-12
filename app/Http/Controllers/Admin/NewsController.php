6a1e2457504bd9096a1031a705a19ca88cefcfa7
sergeykotenko <sergeykotenko@bk.ru>
2021-09-12 14:40:40 +0300

    start form

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use phpDocumentor\Reflection\DocBlock\Description;
 
 class NewsController extends Controller
 {
@@ -26,7 +27,7 @@ class NewsController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.news.create');
     }
 
     /**
@@ -37,7 +38,10 @@ class NewsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => ['required', 'string', 'min:3']
+        ]);
+        dd($request->all());
     }
 
     /**
--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -12,7 +12,7 @@
     <hr class="sidebar-divider my-0">
 
     <!-- Nav Item - Dashboard -->
-    <li class="nav-item active">
+    <li class="nav-item @if(request()->is('admin')) active @endif">
         <a class="nav-link" href="">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Панель управления</span></a>
@@ -20,12 +20,12 @@
 
     <!-- Divider -->
     <hr class="sidebar-divider">
-    <li class="nav-item active">
+    <li class="nav-item @if(request()->routeIs('admin.categories.*')) active @endif">
         <a class="nav-link" href="{{ route('admin.categories.index')  }}">
             <i class="fas fa-fw fa-list-alt"></i>
             <span>Категории</span></a>
     </li>
-    <li class="nav-item active">
+    <li class="nav-item @if(request()->routeIs('admin.news.*')) active @endif">
         <a class="nav-link" href="{{ route('admin.news.index')  }}">
             <i class="fas fa-fw fa-list"></i>
             <span>Новости</span></a>
